65f70b2f17cce83c134c8a9b33369c30bc2c4dd5
sz97 <fcat48241@gmail.com>
2021-02-24 10:47:02 +0600

    Tasbih Notification Scheduled

--- app/src/main/java/com/example/mohaasaba/ibadah/tasbih/FragmentTasbih.java
+++ app/src/main/java/com/example/mohaasaba/ibadah/tasbih/FragmentTasbih.java
@@ -1,5 +1,7 @@
 package com.example.mohaasaba.ibadah.tasbih;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,17 +11,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import com.example.mohaasaba.R;
+import com.example.mohaasaba.receivers.NotificationScheduler;
 
 import java.util.List;
+import java.util.Objects;
 
 
 public class FragmentTasbih extends Fragment {
+    private static final String TAG = FragmentTasbih.class.getCanonicalName();
     private RecyclerView recyclerView;
     private TasbihAdapter tasbihAdapter;
     private Toolbar toolbar;
@@ -59,6 +65,23 @@ public class FragmentTasbih extends Fragment {
         LiveData<List<Tasbih>> tasbihLiveData = tasbihRepository.getAllTasbih();
 
         tasbihLiveData.observe(getViewLifecycleOwner(), tasbihAdapter::submitList);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+        Log.d(TAG, "rescheduleNotification: called");
+        Intent intent = new Intent(getContext(), NotificationScheduler.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), NotificationScheduler.MIDNIGHT_REQUEST_PID,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        try {
+            pendingIntent.send(Objects.requireNonNull(getContext()).getApplicationContext(), 10099, intent);
+            Log.d(TAG, "rescheduleNotification: done");
+        } catch (PendingIntent.CanceledException e) {
+            Log.d(TAG, "rescheduleNotification: failed");
+            e.printStackTrace();
+        }
     }
 
 
